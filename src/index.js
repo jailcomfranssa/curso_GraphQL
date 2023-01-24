dcd39ef3641e708883c951c108a96c5ede951856
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-24 14:14:57 -0300

    criando retorno de tipo basico

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      id: () => '',
+      name: () => '',
+      age: () => 30,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
